7f2b8949fe4cf9fb04092367b59cb09b9b8ba011
DaoChaShao <27882272@qq.com>
2025-09-24 13:31:27 +0800

    feat: enhance home.py with introduction and functionality overview for CNN model

--- subpages/home.py
+++ subpages/home.py
@@ -5,3 +5,16 @@
 # @Version  :   Version 0.1.0
 # @File     :   home.py
 # @Desc     :
+
+from streamlit import title, expander, caption, empty
+
+empty_message = empty()
+empty_message.info("Please check the details at the different pages of core functions.")
+
+title("Convolutional Neural Network (CNN) for MNIST Digit Classification")
+with expander("**INTRODUCTION**", expanded=True):
+    caption("+ 📂 Load MNIST dataset and preprocess for model training.")
+    caption("+ 🧠 Train a Multi-Layer Perceptron with custom epochs and batch size.")
+    caption("+ 📊 Visualize training metrics in real-time (loss, accuracy, precision, recall, AUC).")
+    caption("+ 🧪 Test the trained model on the MNIST test dataset.")
+    caption("+ ✏️ Draw digits on a canvas and get instant predictions.")
